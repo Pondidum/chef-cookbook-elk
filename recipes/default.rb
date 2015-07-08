90cb15f27c3f138b4014633736b326631d02958d
Andy <andy@andydote.co.uk>
2015-07-08 15:58:25 +0000

    elk: install elastic search

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,16 @@
 #
 
 include_recipe 'java'
+
+apt_repository 'elasticsearch' do
+  uri "http://packages.elastic.co/elasticsearch/1.6/debian"
+  components ["stable", "main"]
+  key "https://packages.elastic.co/GPG-KEY-elasticsearch"
+  action :add
+end
+
+package "elasticsearch"
+
+service "elasticsearch" do
+  action [:enable, :start]
+end
