3955bb48a8d34cd48e2c31d9bbd93d0ac1d61ec3
andy <andy@andydote.co.uk>
2015-07-10 18:14:48 +0100

    elk: add nginx

--- recipes/default.rb
+++ recipes/default.rb
@@ -63,3 +63,19 @@ service 'kibana' do
   supports :status => true, :restart => true
   action [:enable, :start]
 end
+
+
+
+package "nginx"
+package "apache2-utils"
+
+template '/etc/nginx/sites-available/default' do
+  source 'nginx.erb'
+  notifies :reload, "service[nginx]"
+  action :create
+end
+
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
