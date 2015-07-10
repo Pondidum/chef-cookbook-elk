239de18553f1f28bff653d4753eeaf977c67dad4
andy <andy@andydote.co.uk>
2015-07-10 16:41:37 +0000

    elk: fix kibana setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -31,29 +31,31 @@ service "elasticsearch" do
   action [:enable, :start]
 end
 
-kibana_source_path = '/tmp/kibana.tar.gz'
-remote_file kibana_source_path do
+kibana_tar = '/tmp/kibana.tar.gz'
+
+remote_file kibana_tar do
   source 'https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz'
   action :create
 end
 
 bash 'extract_kibana' do
-  cwd ::File.dirname(kibana_source_path)
+  cwd ::File.dirname(kibana_tar)
   code <<-EOH
     tar xvf kibana.tar.gz
-    rm /tmp/kibana*/config/kibana.yml
+    rm kibana*/config/kibana.yml
     sudo mkdir -p /opt/kibana
-    sudo cp -R ~/kibana-4*/* /opt/kibana/
-    rm -rf /tmp/kibana*/
+    sudo cp -R kibana-4*/* /opt/kibana/
+    rm -rf kibana*/
   EOH
 end
 
 remote_file '/etc/init.d/kibana' do
   source 'https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4'
+  mode '777'
   action :create
 end
 
-template 'opt/kibana/config/kibana.yml' do
+template '/opt/kibana/config/kibana.yml' do
   notifies :restart, "service[kibana]"
 end
 
